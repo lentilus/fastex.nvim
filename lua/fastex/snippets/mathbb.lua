509189ad1e2ad0d8f60f022bebf5375a96cae496
lentilus <lentilus@mailbox.org>
2024-10-24 10:49:16 +0200

    add \\N and mathcal

--- lua/fastex/snippets/mathbb.lua
+++ lua/fastex/snippets/mathbb.lua
@@ -7,15 +7,17 @@ local math = helper.math
 local cap = helper.cap
 
 return {
-    snip("([A-Z])%1", "\\mathbb{<>}", { cap(1) }, math, 100),
+	snip("([A-Z])%1", "\\mathbb{<>}", { cap(1) }, math, 100),
+	snip("[A-Z]ca", "\\mathcal{<>}", { cap(1) }, math, 100),
 
-    -- higher priority by default:
-    snip("CC", "\\C ", {}, math),
-    snip("KK", "\\K ", {}, math),
-    snip("QQ", "\\Q ", {}, math),
-    snip("RR", "\\R ", {}, math),
-    snip("ZZ", "\\Z ", {}, math),
-    snip("La", "\\La ", {}, math),
+	-- higher priority by default:
+	snip("CC", "\\C ", {}, math),
+	snip("KK", "\\K ", {}, math),
+	snip("QQ", "\\Q ", {}, math),
+	snip("RR", "\\R ", {}, math),
+	snip("ZZ", "\\Z ", {}, math),
+	snip("NN", "\\N ", {}, math),
+	snip("La", "\\La ", {}, math),
 
-    snip("(\\%a)%s?(%d)%s", "<>^<> ", {cap(1), cap(2)}, math),
+	snip("(\\%a)%s?(%d)%s", "<>^<> ", { cap(1), cap(2) }, math),
 }
